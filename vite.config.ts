1f18d3cbe0739f9e593306e0825e2b31e56d5871
Balaji Asish <balajiashish02@gmail.com>
2025-03-15 13:12:08 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), tailwindcss()],
-   resolve: {
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
